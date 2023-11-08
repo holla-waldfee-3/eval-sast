6e0ae7f97063d5b1e8f72b5137201497f3d64b21
holla-waldfee-2 <116636929+holla-waldfee-2@users.noreply.github.com>
2023-11-08 15:52:20 +0100

    Split into two files

--- src/main/java/com/acme/foo/Bar.java
+++ src/main/java/com/acme/foo/Bar.java
@@ -1,10 +1,25 @@
 package com.acme.foo;
 
+import java.net.HttpCookie;
+
 public class Bar {
     
     public static final boolean F = true;
 
     public static class Cookie {
         public Cookie(String bla, String blu) {}
+    }
+
+    public void f6(String... args) {
+        Cookie c = new Cookie("key", "f6"); // Neg - It is not javax.servlet.http.Cookie
+    }
+
+    public void f7() {
+        HttpCookie c = new HttpCookie("key", "f7"); // Pos
+    }
+
+    public void f8() {
+        javax.servlet.http.Cookie c = new javax.servlet.http.Cookie("key", "f8"); // Neg - Foo.F is a constant with value true
+        c.setSecure(Bar.F);
     }
 }
